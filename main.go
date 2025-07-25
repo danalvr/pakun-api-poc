23c5729bc1660ccbd1c189d3d428f91e2a048db1
Daniel Alvaro <danielkisaran@gmail.com>
2025-07-25 23:38:14 +0700

    feat: adding setup cors.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"pakun-api-poc/firebase"
 	"pakun-api-poc/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,15 @@ func main() {
 	defer firebase.Client.Close()
 
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5174"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.RegisterRoutes(server)
 
